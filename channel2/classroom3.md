accda40eaf70d991e322887bcd918d5cf872ba49
benjamin <benjamin@benjamindeMacBook-Air.local>
2022-10-29 14:16:22 +0800

    a

---
+++ channel2/classroom3.md
@@ -0,0 +1,39 @@
+
+### [第一部分](#过奖了)
+#### [过奖了](#过奖了)
+#### [你是最棒的](#你是最棒的)
+#### [我被感动了](#我被感动了)
+#### [这部电影很感人](#这部电影很感人)
+#### [我紧张](#我紧张)
+#### [我在出汗](#我在出汗)
+#### [我浑身都是汗](#我浑身都是汗)
+#### [小菜一碟啦](#小菜一碟啦)
+
+
+
+
+
+## 过奖了 
+I'm flattered 
+## 你是最棒的 
+You are the best
+## 我被感动了
+I'm touched
+## 这部电影很感人
+This movie is touching
+## 我紧张
+I'm nervous
+Relax! 放松
+## 我在出汗
+I'm sweating
+## 我浑身都是汗
+I'm sweating a lot
+## 小菜一碟啦
+No sweat
+Can you eat up the food?
+No sweat
+eat up  吃完
+## 我好无聊哦……
+I'm bored
+I'm bored to death
+This movie is boring, I want to go
--- main.go
+++ main.go
@@ -2,25 +2,62 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	m:=map[string]string{
-		"":""
+	type Sentents struct {
+		English string `json:"english,omitempty"`
+		Chinese string `json:"chinese,omitempty"`
+		Comment string `json:"comment,omitempty"`
 	}
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("input start begin game or input q over game")
+
+	file, err := os.Open("abc.json")
+	if err != nil {
+		panic(err)
+	}
+	body, err := ioutil.ReadAll(file)
+	if err != nil {
+		file.Close()
+		panic(err)
+	}
+	file.Close()
+	var ss []Sentents
+	err = json.Unmarshal(body, &ss)
+	if err != nil {
+		panic(err)
+	}
 	for {
-	
-		result, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("read error:", err)
-			continue
+		fmt.Println("1. input english \n2. input chinese")
+		input := ""
+		fmt.Scan(&input)
+		fmt.Printf("you input %s\n", input)
+		if input == "1" {
+			bf := bufio.NewReader(os.Stdin)
+			for {
+				line, _, _ := bf.ReadLine()
+				if string(line) == "q" {
+					break
+				}
+				ss = append(ss, Sentents{English: string(line)})
+				continue
+			}
+
 		}
-		fmt.Println("result:", result)
-		if result == "q\n" || result == "quit" {
+		// var chinese, english, comment string
+		var chinese string
+		fmt.Println("plesese input")
+		fmt.Scanln(&chinese)
+		if chinese == "q" {
+			b, err := json.Marshal(&ss)
+			if err != nil {
+				panic(err)
+			}
+			ioutil.WriteFile("abc.json", b, fs.ModePerm)
 			break
 		}
 	}
